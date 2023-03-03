1ed5c382d88321348859f81ae1aca68b508cec62
Ankush Ahuja <aahuja2006@gmail.com>
2023-03-03 19:49:51 +0100

    finished line tracker - calib. needed

---
+++ ankush_triple_follower.c
@@ -0,0 +1,11 @@
+// motors
+#define MOTOR_LEFT
+#define MOTOR_RIGHT
+
+// sensors
+#define SENSOR_LEFT
+#define SENSOR_RIGHT
+#define SENSOR_SIDE
+
+// sensor data struct
+//
--- main.c
+++ main.c
@@ -5,154 +5,135 @@
 #define LEFT_SENSOR 1
 #define RIGHT_SENSOR 0
 
-struct sensor_data {
-  int sensor_side_black;
-  int sensor_side_white;
+struct sensor_data{
+    int sensor_side_black;
+    int sensor_side_white;
 
-  int sensor_left_black;
-  int sensor_left_white;
+    int sensor_left_black;
+    int sensor_left_white;
 
-  int sensor_right_black;
-  int sensor_right_white;
+	int sensor_right_black;
+    int sensor_right_white;
 };
 
-struct sensor_data calibrate_sensors() {
-  struct sensor_data sensors;
+struct sensor_data calibrate_sensors(){
+    struct sensor_data sensors;
 
-  printf("%s\n", "Put both sensors on the line.");
-  while (1)
-    if (a_button_clicked())
-      break;
-
-  sensors.sensor_side_black = analog(SIDE_SENSOR);
-  sensors.sensor_left_black = analog(LEFT_SENSOR);
-  sensors.sensor_right_black = analog(RIGHT_SENSOR);
-
-  printf("%s\n", "Put both sensors out of the line.");
-  while (1)
-    if (a_button_clicked())
-      break;
-
-  sensors.sensor_side_white = analog(SIDE_SENSOR);
-  sensors.sensor_left_white = analog(LEFT_SENSOR);
-  sensors.sensor_right_white = analog(RIGHT_SENSOR);
-
-  printf("Calibrated:\n\tSensor_Side: %d, %d\n\tSensor_Left: %d, "
-         "%d\n\tSensor_Right: %d, %d\n",
-         sensors.sensor_side_black, sensors.sensor_side_white,
-         sensors.sensor_left_black, sensors.sensor_left_white,
-         sensors.sensor_right_black, sensors.sensor_right_white);
-
-  /*
-  printf("%s\n", "Put both sensors on the line.");
-  while(1)
-      if(a_button_clicked())
-          break;
-
-  sensors.sensor_0_black = analog(LIGHT_SENSOR_0);
-  sensors.sensor_1_black = analog(LIGHT_SENSOR_1);
-
-  printf("%s\n", "Put both sensors outside the line.");
-  while(1)
-      if(a_button_clicked())
-          break;
-
-  sensors.sensor_0_white = analog(LIGHT_SENSOR_0);
-  sensors.sensor_1_white = analog(LIGHT_SENSOR_1);
-
-  printf("Calibrated:\n\tSensor 0: %d, %d\n\tSensor 1: %d, %d\n",
-  sensors.sensor_0_black, sensors.sensor_0_white, sensors.sensor_1_black,
-  sensors.sensor_1_white);
-      */
-
-  return sensors;
+    printf("%s\n", "Put both sensors on the line.");
+    while(1)
+        if(a_button_clicked())
+            break;
+    
+    sensors.sensor_side_black = analog(SIDE_SENSOR);
+    sensors.sensor_left_black = analog(LEFT_SENSOR);
+    sensors.sensor_right_black = analog(RIGHT_SENSOR);
+    
+    printf("%s\n", "Put both sensors out of the line.");
+    while(1)
+        if(a_button_clicked())
+            break;
+    
+    sensors.sensor_side_white = analog(SIDE_SENSOR);
+    sensors.sensor_left_white = analog(LEFT_SENSOR);
+    sensors.sensor_right_white = analog(RIGHT_SENSOR);
+    
+    printf("Calibrated:\n\tSensor_Side: %d, %d\n\tSensor_Left: %d, %d\n\tSensor_Right: %d, %d\n", sensors.sensor_side_black, sensors.sensor_side_white, sensors.sensor_left_black, sensors.sensor_left_white, sensors.sensor_right_black, sensors.sensor_right_white);
+    
+    return sensors;
 }
 
-void freezeB() {
-  freeze(0);
-  freeze(1);
+void freezeB(){
+    freeze(0);
+    freeze(1);
 }
 
-int main() {
+// ALL GAS NO BRAKES
+void AGNB(){
+    fd(0); // LEFT
+    fd(1); // RIGHT
+}
 
-  struct sensor_data sensors = calibrate_sensors();
+void turnRight(){
+    freezeB();
+    
+	mav(0, 1500);
+    mav(1, -1500);
+    
+    msleep(800);
+    
+    freezeB();
+}
 
-  sleep(5);
+void turnLeft(){
+    freezeB();
+    
+	mav(0, -1500);
+    mav(1, 1500);
+    
+    msleep(800);
+    
+    freezeB();
+}
 
-  fd(0); // LEFT
-  fd(1); // RIGHT
+int main(){
+	
+    struct sensor_data sensors = calibrate_sensors();
+    msleep(5000);
+    
+    /*
+    struct sensor_data sensors;
+    sensors.sensor_side_white = 162;
+    sensors.sensor_right_white = 1176;
+	sensors.sensor_left_white = 1247;
+    */
+    
+    int intersection = 0;
+    int onIntersection = 0;
+    
+    //sleep(5);
+    
+	AGNB();
+    
+    while(1){
+        
+        if(analog(LEFT_SENSOR) < sensors.sensor_left_white+200){
+            freezeB();
+            
+            mav(0,1500);
+            
+            while(!((analog(LEFT_SENSOR) >= sensors.sensor_left_white+200) && (analog(RIGHT_SENSOR) >= sensors.sensor_right_white+200)));
+            
+			AGNB();
+        }
+            
+        if(analog(RIGHT_SENSOR) < sensors.sensor_left_white+200){
+            freezeB();
+            
+            mav(1, 1500);
 
-  // FD SLOWS THE ROBOT?
-
-  while (1) {
-
-    if (analog(LEFT_SENSOR) < sensors.sensor_left_white + 200) {
-      freezeB();
-
-      mav(0, 1500);
-      // mav(1, 1300);
-
-      while (!((analog(LEFT_SENSOR) >= sensors.sensor_left_white + 200) &&
-               (analog(RIGHT_SENSOR) >= sensors.sensor_right_white + 200)))
-        ;
-
-      fd(0);
-      fd(1);
-
-      // printf("%s", "LEFT OUTSIDE");
-      // break;
+            while(!((analog(LEFT_SENSOR) >= sensors.sensor_left_white+200) && (analog(RIGHT_SENSOR) >= sensors.sensor_right_white+200)));
+            
+			AGNB();
+        }
+        
+        if(analog(SIDE_SENSOR) >= (sensors.sensor_side_white+200)){
+            if(!onIntersection){
+                intersection++;
+            	printf("Intersections found: %d", intersection);
+			}
+            onIntersection++;
+        }else{
+        	onIntersection = 0;
+        }
+        
+        if(intersection == 2){
+            
+            msleep(580);
+            
+        	turnRight();
+        }
     }
-
-    if (analog(RIGHT_SENSOR) < sensors.sensor_left_white + 200) {
-      freezeB();
-
-      // mav(0,1300);
-      mav(1, 1500);
-
-      while (!((analog(LEFT_SENSOR) >= sensors.sensor_left_white + 200) &&
-               (analog(RIGHT_SENSOR) >= sensors.sensor_right_white + 200)))
-        ;
-
-      fd(0);
-      fd(1);
-
-      // printf("%s", "RIGHT OUTSIDE");
-      //  break;
-    }
-  }
-
-  // printf("LEFT:  %d, ON LINE: %s\n", analog(LEFT_SENSOR), analog(LEFT_SENSOR)
-  // >= sensors.sensor_left_white+200 ? "YES" : "NO"); printf("RIGHT: %d, ON
-  // LINE: %s\n", analog(RIGHT_SENSOR), analog(RIGHT_SENSOR) >=
-  // sensors.sensor_left_white+200 ? "YES" : "NO"); printf("SIDE:  %d, OUTSIDE
-  // LINE: %s\n", analog(SIDE_SENSOR), analog(SIDE_SENSOR) <=
-  // sensors.sensor_side_white+200 ? "YES" : "NO");
-
-  // sleep(2);
-
-  // if(analog(LEFT_SENSOR) >= sensors.sensor_left_white+200){
-  //	mav(1, 500);
-  //	while(!((analog(LEFT_SENSOR) >= sensors.sensor_left_white+200) &&
-  //(analog(RIGHT_SENSOR) >= sensors.sensor_right_white+200)));
-  // }
-
-  // if(analog(RIGHT_SENSOR) >= sensors.sensor_right_white+200){
-  //	mav(0, 500);
-  //     while(!((analog(LEFT_SENSOR) >= sensors.sensor_left_white+200) &&
-  //     (analog(RIGHT_SENSOR) >= sensors.sensor_right_white+200)));
-  // }
-
-  // fd(0);
-  // fd(1);
-
-  /*
-  while((analog(LEFT_SENSOR) >= sensors.sensor_left_white+200) &&
-  (analog(RIGHT_SENSOR) >= sensors.sensor_right_white+200)){
-      if(analog(SIDE_SENSOR) >= sensors.sensor_side_white+200){
-          break;
-      }
-  }
-*/
-
-  return 0;
+    
+    return 0;
 }
+
